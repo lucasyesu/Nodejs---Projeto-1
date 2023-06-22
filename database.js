9135829f4bd4996a438699ffb4a15ca4785a0caa
Lucas <129877848+lucasyesu@users.noreply.github.com>
2023-06-22 20:01:46 -0300

    Update database.js

--- database.js
+++ database.js
@@ -5,7 +5,17 @@ export class Database {
 
     select(table) {
         const data = this.database[table] ?? []
+
+        return data
     }
 
-    insert(table, data) {}
+    insert(table, data) {
+        if (Array.isArray(this.database[table])) {
+            this.database[table].push(data)
+        } else {
+            this.database[table] = [data]
+        }
+
+        return data;
+    }
 }
